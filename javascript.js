7088fbbac490b0c837808494fb227b03dcab3ba7
reiogo <raogoda2@gmail.com>
2024-06-11 21:03:47 +0900

    Add interface with math functions and variables
    Made math functions and variables for the input number and the
    operator. Needed an interface for it, so added it.

--- javascript.js
+++ javascript.js
@@ -1,4 +1,4 @@
-//functions for calculators
+// Math functions.
 const add = function(arg1, arg2) {
     return arg1 + arg2;
 	
@@ -16,5 +16,31 @@ const multiply = function(arg1, arg2) {
 const divide = function(arg1, arg2){
     return arg1/arg2;
 };
+
+
+// Variables for the first number, second number, and operator.
+
+let argOne = 0;
+let argTwo = 0;
+let operator = '';
+
+// Interface for variables and math functions.
+function operate (argOne, argTwo, operator) {
+    switch (operator) {
+        case 'add': 
+            add(argOne, argTwo);
+            break;
+        case 'substract': 
+            substract(argOne, argTwo);
+            break;
+        case 'multiply': 
+            multiply(argOne, argTwo);
+            break;
+        case 'divide': 
+            divide(argOne, argTwo);
+            break;
+        default:
+            alert ('Something went wrong');
+    }
 
 
