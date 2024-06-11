e9d1818d2fcae511354f1651bc5046f8a7d7896b
reiogo <raogoda2@gmail.com>
2024-06-11 20:24:26 +0900

    Add boilerplate code
    In the javascript, there are already math functions which are also
    considered basic.

---
+++ javascript.js
@@ -0,0 +1,20 @@
+//functions for calculators
+const add = function(arg1, arg2) {
+    return arg1 + arg2;
+	
+};
+
+const subtract = function(arg1, arg2) {
+    return arg1 - arg2;
+	
+};
+
+const multiply = function(arg1, arg2) {
+    return arg1*arg2;
+};
+
+const divide = function(arg1, arg2){
+    return arg1/arg2;
+};
+
+
